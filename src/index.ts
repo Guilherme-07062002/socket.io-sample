d8cba0005fb6c18d3d97fa586c9392682413ab9f
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-24 10:22:58 -0300

    feat: express api sample hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world!')
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  const response = JSON.stringify({
+    message: 'Hello World'
+  });
+  res.send(response);
+});
+
+server.listen(3000, () => {
+  console.log('\nlistening on 3000');
+});
