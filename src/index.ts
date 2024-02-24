618237462e0e7a6579a11c854a9e8756dea01104
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-24 11:25:04 -0300

    feat: update index.html file to create a new socket io instance

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,6 @@ const io = new Server(server);
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
 });
 
 app.get('/', (req, res) => {
